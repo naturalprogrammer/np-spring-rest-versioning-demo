fef5116bbdd7cda7e42422bc38ef5a2b5c06cb3e
Sanjay Patel <skpatel20@gmail.com>
2023-02-26 08:25:13 +0530

    MIME types defined in DTOs

--- src/main/java/com/naturalprogrammer/restversion/signup/SignupController.java
+++ src/main/java/com/naturalprogrammer/restversion/signup/SignupController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SignupController {
 
-    public record SignupRequestDto(String email, String password) {}
-    public record UserResourceDto(String email) {}
+    public record SignupRequestDto(String email, String password) {
+        public static final String MEDIA_TYPE = "application/vnd.com.example.signup-request+json; version=1.0";
+    }
+
+    public record UserResourceDto(String email) {
+        public static final String MEDIA_TYPE = "application/vnd.com.example.user+json; version=1.0";
+    }
 
     @PostMapping(
             value = "/users",
-            consumes = "application/vnd.com.example.signup-request+json; version=1.0",
-            produces = "application/vnd.com.example.user+json; version=1.0"
+            consumes = SignupRequestDto.MEDIA_TYPE,
+            produces = UserResourceDto.MEDIA_TYPE
     )
     public UserResourceDto signup(@RequestBody SignupRequestDto requestDto) {
         return new UserResourceDto(requestDto.email());
